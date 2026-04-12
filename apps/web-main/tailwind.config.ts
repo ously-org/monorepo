236904138b350de0060ea525658e82b1194d779c
preamza02 <preamza02@gmail.com>
2026-04-12 14:16:30 +0700

    feat: Next.js apps scaffold (web-main, web-prosper)

---
+++ apps/web-main/tailwind.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from "tailwindcss";
+import sharedConfig from "@ously/config-tailwind";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "../../packages/ui/src/**/*.{ts,tsx}",
+  ],
+  presets: [sharedConfig as any],
+};
+export default config;
