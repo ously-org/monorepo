9ee6de72c256c6fb5d5366ce60ab4abe83a1a2e3
preamza02 <preamza02@gmail.com>
2026-04-21 23:41:33 +0700

    fix(ods): align chart imports and exports with architectural standards

--- packages/ods/src/components/chart.tsx
+++ packages/ods/src/components/chart.tsx
@@ -2,10 +2,14 @@ import {
   ChartContainer as InternalChartContainer, 
   ChartTooltip as InternalChartTooltip, 
   ChartTooltipContent as InternalChartTooltipContent,
+  ChartLegend as InternalChartLegend,
+  ChartLegendContent as InternalChartLegendContent,
   type ChartConfig
 } from "../internal/chart"
 
 export const ChartContainer = InternalChartContainer
 export const ChartTooltip = InternalChartTooltip
 export const ChartTooltipContent = InternalChartTooltipContent
+export const ChartLegend = InternalChartLegend
+export const ChartLegendContent = InternalChartLegendContent
 export type { ChartConfig }
